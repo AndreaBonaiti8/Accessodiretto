d2189e8e98bdf0b7a40b4b2ef414cdb06f1b6efe
Bonaiti.20104 <Bonaiti.20104@LABI2-13PC13>
2023-11-07 11:17:24 +0100

    Aggiungere i file di progetto.

---
+++ Accessodiretto/Form1.Designer.cs
@@ -0,0 +1,321 @@
+﻿namespace Accessodiretto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.Aggiunta = new System.Windows.Forms.Button();
+            this.cerca = new System.Windows.Forms.Button();
+            this.visualizza = new System.Windows.Forms.Button();
+            this.modifica = new System.Windows.Forms.Button();
+            this.reset = new System.Windows.Forms.Button();
+            this.cancfisic = new System.Windows.Forms.Button();
+            this.recuperare = new System.Windows.Forms.Button();
+            this.cancellalogic = new System.Windows.Forms.Button();
+            this.proddacerc = new System.Windows.Forms.TextBox();
+            this.prezdamod = new System.Windows.Forms.TextBox();
+            this.proddamod = new System.Windows.Forms.TextBox();
+            this.prezzo = new System.Windows.Forms.TextBox();
+            this.prodotto = new System.Windows.Forms.TextBox();
+            this.proddacanclogic = new System.Windows.Forms.TextBox();
+            this.proddacancfisic = new System.Windows.Forms.TextBox();
+            this.proddarecu = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(229, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "cerca prodotto";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(198, 438);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "recupera prodotto";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(24, 438);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(103, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "cancellazione logica";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(627, 19);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "prezzo";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(107, 19);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(434, 438);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(99, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "cancellazione fisica";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(529, 19);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(46, 13);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "prodotto";
+            // 
+            // Aggiunta
+            // 
+            this.Aggiunta.Location = new System.Drawing.Point(12, 61);
+            this.Aggiunta.Name = "Aggiunta";
+            this.Aggiunta.Size = new System.Drawing.Size(160, 42);
+            this.Aggiunta.TabIndex = 8;
+            this.Aggiunta.Text = "button1";
+            this.Aggiunta.UseVisualStyleBackColor = true;
+            // 
+            // cerca
+            // 
+            this.cerca.Location = new System.Drawing.Point(227, 61);
+            this.cerca.Name = "cerca";
+            this.cerca.Size = new System.Drawing.Size(215, 42);
+            this.cerca.TabIndex = 9;
+            this.cerca.Text = "cerca";
+            this.cerca.UseVisualStyleBackColor = true;
+            // 
+            // visualizza
+            // 
+            this.visualizza.Location = new System.Drawing.Point(201, 305);
+            this.visualizza.Name = "visualizza";
+            this.visualizza.Size = new System.Drawing.Size(178, 42);
+            this.visualizza.TabIndex = 10;
+            this.visualizza.Text = "Visualizza file";
+            this.visualizza.UseVisualStyleBackColor = true;
+            // 
+            // modifica
+            // 
+            this.modifica.Location = new System.Drawing.Point(532, 61);
+            this.modifica.Name = "modifica";
+            this.modifica.Size = new System.Drawing.Size(165, 42);
+            this.modifica.TabIndex = 11;
+            this.modifica.Text = "button4";
+            this.modifica.UseVisualStyleBackColor = true;
+            // 
+            // reset
+            // 
+            this.reset.Location = new System.Drawing.Point(27, 305);
+            this.reset.Name = "reset";
+            this.reset.Size = new System.Drawing.Size(153, 42);
+            this.reset.TabIndex = 12;
+            this.reset.Text = "button5";
+            this.reset.UseVisualStyleBackColor = true;
+            // 
+            // cancfisic
+            // 
+            this.cancfisic.Location = new System.Drawing.Point(437, 486);
+            this.cancfisic.Name = "cancfisic";
+            this.cancfisic.Size = new System.Drawing.Size(220, 42);
+            this.cancfisic.TabIndex = 13;
+            this.cancfisic.Text = "Cancellazione fisica";
+            this.cancfisic.UseVisualStyleBackColor = true;
+            // 
+            // recuperare
+            // 
+            this.recuperare.Location = new System.Drawing.Point(201, 486);
+            this.recuperare.Name = "recuperare";
+            this.recuperare.Size = new System.Drawing.Size(192, 42);
+            this.recuperare.TabIndex = 14;
+            this.recuperare.Text = "recupera";
+            this.recuperare.UseVisualStyleBackColor = true;
+            // 
+            // cancellalogic
+            // 
+            this.cancellalogic.Location = new System.Drawing.Point(27, 486);
+            this.cancellalogic.Name = "cancellalogic";
+            this.cancellalogic.Size = new System.Drawing.Size(124, 42);
+            this.cancellalogic.TabIndex = 15;
+            this.cancellalogic.Text = "cancellazione logica";
+            this.cancellalogic.UseVisualStyleBackColor = true;
+            // 
+            // proddacerc
+            // 
+            this.proddacerc.Location = new System.Drawing.Point(232, 35);
+            this.proddacerc.Name = "proddacerc";
+            this.proddacerc.Size = new System.Drawing.Size(210, 20);
+            this.proddacerc.TabIndex = 16;
+            // 
+            // prezdamod
+            // 
+            this.prezdamod.Location = new System.Drawing.Point(630, 35);
+            this.prezdamod.Name = "prezdamod";
+            this.prezdamod.Size = new System.Drawing.Size(67, 20);
+            this.prezdamod.TabIndex = 17;
+            // 
+            // proddamod
+            // 
+            this.proddamod.Location = new System.Drawing.Point(532, 35);
+            this.proddamod.Name = "proddamod";
+            this.proddamod.Size = new System.Drawing.Size(92, 20);
+            this.proddamod.TabIndex = 18;
+            // 
+            // prezzo
+            // 
+            this.prezzo.Location = new System.Drawing.Point(110, 35);
+            this.prezzo.Name = "prezzo";
+            this.prezzo.Size = new System.Drawing.Size(62, 20);
+            this.prezzo.TabIndex = 19;
+            // 
+            // prodotto
+            // 
+            this.prodotto.Location = new System.Drawing.Point(12, 35);
+            this.prodotto.Name = "prodotto";
+            this.prodotto.Size = new System.Drawing.Size(92, 20);
+            this.prodotto.TabIndex = 20;
+            // 
+            // proddacanclogic
+            // 
+            this.proddacanclogic.Location = new System.Drawing.Point(27, 460);
+            this.proddacanclogic.Name = "proddacanclogic";
+            this.proddacanclogic.Size = new System.Drawing.Size(124, 20);
+            this.proddacanclogic.TabIndex = 21;
+            // 
+            // proddacancfisic
+            // 
+            this.proddacancfisic.Location = new System.Drawing.Point(437, 460);
+            this.proddacancfisic.Name = "proddacancfisic";
+            this.proddacancfisic.Size = new System.Drawing.Size(169, 20);
+            this.proddacancfisic.TabIndex = 22;
+            // 
+            // proddarecu
+            // 
+            this.proddarecu.Location = new System.Drawing.Point(201, 460);
+            this.proddarecu.Name = "proddarecu";
+            this.proddarecu.Size = new System.Drawing.Size(178, 20);
+            this.proddarecu.TabIndex = 23;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(963, 607);
+            this.Controls.Add(this.proddarecu);
+            this.Controls.Add(this.proddacancfisic);
+            this.Controls.Add(this.proddacanclogic);
+            this.Controls.Add(this.prodotto);
+            this.Controls.Add(this.prezzo);
+            this.Controls.Add(this.proddamod);
+            this.Controls.Add(this.prezdamod);
+            this.Controls.Add(this.proddacerc);
+            this.Controls.Add(this.cancellalogic);
+            this.Controls.Add(this.recuperare);
+            this.Controls.Add(this.cancfisic);
+            this.Controls.Add(this.reset);
+            this.Controls.Add(this.modifica);
+            this.Controls.Add(this.visualizza);
+            this.Controls.Add(this.cerca);
+            this.Controls.Add(this.Aggiunta);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button Aggiunta;
+        private System.Windows.Forms.Button cerca;
+        private System.Windows.Forms.Button visualizza;
+        private System.Windows.Forms.Button modifica;
+        private System.Windows.Forms.Button reset;
+        private System.Windows.Forms.Button cancfisic;
+        private System.Windows.Forms.Button recuperare;
+        private System.Windows.Forms.Button cancellalogic;
+        private System.Windows.Forms.TextBox proddacerc;
+        private System.Windows.Forms.TextBox prezdamod;
+        private System.Windows.Forms.TextBox proddamod;
+        private System.Windows.Forms.TextBox prezzo;
+        private System.Windows.Forms.TextBox prodotto;
+        private System.Windows.Forms.TextBox proddacanclogic;
+        private System.Windows.Forms.TextBox proddacancfisic;
+        private System.Windows.Forms.TextBox proddarecu;
+    }
+}
+
---
+++ Accessodiretto/Form1.cs
@@ -0,0 +1,331 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Accessodiretto
+{
+    public partial class Form1 : Form
+    {
+        int LunghezzaRecord = 64;
+        string Prodottodamod = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public int ricercaindice(string nome)
+        {
+            int riga = 0;
+            using (StreamReader sr = File.OpenText("File.dat"))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    string[] dati = line.Split(';');
+                    if (dati[3] == "0" && dati[0] == nome)
+                    {
+                        sr.Close();
+                        return riga;
+                    }
+                    riga++;
+                }
+            }
+            return -1;
+        }
+        public int ricercaindicecancfisic(string nome)
+        {
+            int riga = 0;
+            using (StreamReader sr = File.OpenText("File.dat"))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    string[] dati = line.Split(';');
+                    if (dati[0] == nome)
+                    {
+                        sr.Close();
+                        return riga;
+                    }
+                    riga++;
+                }
+            }
+            return -1;
+        }
+        public string[] ricercaprod(string nome)
+        {
+            int riga = 0;
+            using (StreamReader sr = File.OpenText("File.dat"))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    string[] dati = line.Split(';');
+                    if (dati[3] == "0" && dati[0] == nome)
+                    {
+                        sr.Close();
+                        return dati;
+                    }
+                    riga++;
+                }
+            }
+            return null;
+        }
+        public string[] ricercaproddarecu(string nome)
+        {
+            int riga = 0;
+            using (StreamReader sr = File.OpenText("File.dat"))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    string[] dati = line.Split(';');
+                    if (dati[3] == "1" && dati[0] == nome)
+                    {
+                        sr.Close();
+                        return dati;
+                    }
+                    riga++;
+                }
+            }
+            return null;
+        }
+        public int ricercaindicedarecu(string nome)
+        {
+            int riga = 0;
+            using (StreamReader sr = File.OpenText("File.dat"))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    string[] dati = line.Split(';');
+                    if (dati[3] == "1" && dati[0] == nome)
+                    {
+                        sr.Close();
+                        return riga;
+                    }
+                    riga++;
+                }
+            }
+            return -1;
+        }
+        public void Aggiunta_Click()
+        {
+            var file = new FileStream("File.dat", FileMode.Append, FileAccess.Write, FileShare.Read);
+            StreamWriter sw = new StreamWriter(file);
+            sw.WriteLine($"{prodotto.Text};{prezzo.Text};1;0;".PadRight(LunghezzaRecord - 4) + "##");
+            sw.Close();
+            prodotto.Text = null;
+            prezzo.Text = null;
+        }
+        private void Aggiungi_Click_1(object sender, EventArgs e)
+        {
+
+            if (prodotto.Text == string.Empty || prezzo.Text == string.Empty)
+            {
+                MessageBox.Show("Devi prima inserire un prodotto e il suo prezzo!", "Cella vuota");
+            }
+            else if (float.TryParse(prezzo.Text, out _) == false)
+            {
+                MessageBox.Show("Devi inserire il prezzo in valori numerici!", "Prezzo in Numero");
+                prezzo.Text = string.Empty;
+            }
+            else
+            {
+                var file = new FileStream("File.dat", FileMode.Append, FileAccess.Write, FileShare.Read);
+                StreamWriter sw = new StreamWriter(file);
+                sw.WriteLine($"{prodotto.Text};{prezzo.Text};1;0;".PadRight(LunghezzaRecord - 4) + "##");
+                sw.Close();
+                prodotto.Text = null;
+                prezzo.Text = null;
+            }
+        }
+        private void reset_Click_1(object sender, EventArgs e)
+        {
+            var file = new FileStream("File.dat", FileMode.Truncate, FileAccess.Write, FileShare.Read);
+            StreamWriter sw = new StreamWriter(file);
+            sw.Write(string.Empty);
+            sw.Close();
+            MessageBox.Show("File Resetato Correttamente", "Fine Operazione");
+        }
+
+        private void cancellalogic_Click(object sender, EventArgs e)
+        {
+            if (proddacanclogic.Text == string.Empty)
+            {
+                MessageBox.Show("Devi prima inserire un prodotto!", "Cella vuota");
+            }
+            else
+            {
+                int indice = ricercaindice(proddacanclogic.Text);
+                if (indice == -1)
+                {
+                    MessageBox.Show("Assicurati che il prodotto esista!", "Prod. non Trovato");
+                    proddacanclogic.Text = null;
+                }
+                else
+                {
+                    string[] prodotto = ricercaprod(proddacanclogic.Text);
+                    string line;
+                    var file = new FileStream("File.dat", FileMode.Open, FileAccess.Write);
+                    BinaryWriter writer = new BinaryWriter(file);
+                    file.Seek(LunghezzaRecord * indice, SeekOrigin.Begin);
+                    line = $"{prodotto[0]};{prodotto[1]};{prodotto[3]};1;".PadRight(LunghezzaRecord - 4) + "##";
+                    byte[] bytes = Encoding.UTF8.GetBytes(line);
+                    writer.Write(bytes, 0, bytes.Length);
+                    writer.Close();
+                    file.Close();
+                    proddacanclogic.Text = null;
+                    MessageBox.Show("Cancellazone Logica Eseguita Correttamente", "Fine Operazione");
+                }
+            }
+        }
+
+        private void cerca_Click_1(object sender, EventArgs e)
+        {
+            if (proddacerc.Text == string.Empty)
+            {
+                MessageBox.Show("Devi prima inserire un prodotto!", "Cella vuota");
+            }
+            else
+            {
+                string[] nomeprod = ricercaprod(proddacerc.Text);
+                if (nomeprod == null)
+                {
+                    MessageBox.Show("Assicurati che il prodotto esista!", "Prod. non Trovato");
+                    proddacerc.Text = null;
+                }
+                else
+                {
+                    proddamod.Text = nomeprod[0];
+                    prezdamod.Text = nomeprod[1];
+                    Prodottodamod = proddacerc.Text;
+                    proddacerc.Text = null;
+                }
+            }
+        }
+
+        private void modifica_Click_1(object sender, EventArgs e)
+        {
+            if (proddamod.Text == string.Empty || prezdamod.Text == string.Empty)
+            {
+                MessageBox.Show("Devi prima inserire un prodotto e il suo prezzo!", "Cella vuota");
+            }
+            else if (float.TryParse(prezdamod.Text, out _) == false)
+            {
+                MessageBox.Show("Devi inserire il prezzo in valori numerici!", "Prezzo in Numero");
+                prezzo.Text = string.Empty;
+            }
+            else
+            {
+                int indice = ricercaindice(Prodottodamod);
+                string line;
+                var file = new FileStream("File.dat", FileMode.Open, FileAccess.Write);
+                BinaryWriter writer = new BinaryWriter(file);
+                file.Seek(LunghezzaRecord * indice, SeekOrigin.Begin);
+                line = $"{proddamod.Text};{prezdamod.Text};1;0;".PadRight(LunghezzaRecord - 4) + "##";
+                byte[] bytes = Encoding.UTF8.GetBytes(line);
+                writer.Write(bytes, 0, bytes.Length);
+                writer.Close();
+                file.Close();
+                proddamod.Text = null;
+                prezdamod.Text = null;
+                Prodottodamod = "";
+                MessageBox.Show("Modifica Eseguita Correttamente", "Fine Operazione");
+            }
+        }
+
+        private void cancfisic_Click_1(object sender, EventArgs e)
+        {
+            if (cancfisic.Text == string.Empty)
+            {
+                MessageBox.Show("Devi prima inserire un prodotto!", "Cella vuota");
+            }
+            else
+            {
+                int indice = ricercaindicecancfisic(cancfisic.Text);
+                if (indice == -1)
+                {
+                    MessageBox.Show("Assicurati che il prodotto esista!", "Prod. non Trovato");
+                    cancfisic.Text = null;
+                }
+                else
+                {
+                    DialogResult result = MessageBox.Show("Sei sicuro di volerlo cancellare? \n(non potra più essere recuperato!)", "Sei sicuro?", MessageBoxButtons.YesNo);
+                    if (result == DialogResult.Yes)
+                    {
+                        string[] line = File.ReadAllLines("File.dat");
+                        for (int i = indice; i < line.Length - 1; i++)
+                        {
+                            line[i] = line[i + 1];
+                        }
+
+                        var file = new FileStream("File.dat", FileMode.Truncate, FileAccess.Write, FileShare.Read);
+                        StreamWriter sw = new StreamWriter(file);
+                        sw.Write(string.Empty);
+                        sw.Close();
+
+                        var files = new FileStream("File.dat", FileMode.Append, FileAccess.Write, FileShare.Read);
+                        StreamWriter sws = new StreamWriter(files);
+                        for (int i = 0; i < line.Length - 1; i++)
+                        {
+                            sws.WriteLine(line[i]);
+                        }
+                        sws.Close();
+                        cancfisic.Text = null;
+                        MessageBox.Show("Cancellazione Fisica Eseguita Correttamente", "Fine Operazione");
+                    }
+                    else
+                    {
+                        cancfisic.Text = null;
+                    }
+                }
+            }
+        }
+
+        private void visualizza_Click_1(object sender, EventArgs e)
+        {
+            string percorsofiles = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "File.dat");
+            Process.Start(percorsofiles);
+        }
+
+        private void recuperare_Click(object sender, EventArgs e)
+        {
+            if (proddarecu.Text == string.Empty)
+            {
+                MessageBox.Show("Devi prima inserire un prodotto!", "Cella vuota");
+            }
+            else
+            {
+                int indice = ricercaindicedarecu(proddarecu.Text);
+                if (indice == -1)
+                {
+                    MessageBox.Show("Assicurati che il prodotto esista e che sia cancellato!", "Prod. non Trovato");
+                    proddarecu.Text = null;
+                }
+                else
+                {
+                    string[] prodotto = ricercaproddarecu(proddarecu.Text);
+                    string line;
+                    var file = new FileStream("File.dat", FileMode.Open, FileAccess.Write);
+                    BinaryWriter writer = new BinaryWriter(file);
+                    file.Seek(LunghezzaRecord * indice, SeekOrigin.Begin);
+                    line = $"{prodotto[0]};{prodotto[1]};1;0;".PadRight(LunghezzaRecord - 4) + "##";
+                    byte[] bytes = Encoding.UTF8.GetBytes(line);
+                    writer.Write(bytes, 0, bytes.Length);
+                    writer.Close();
+                    file.Close();
+                    proddarecu.Text = null;
+                    MessageBox.Show("Prodotto Recuperato Correttamente", "Fine Operazione");
+                }
+            }
+        }
+    }
+}
